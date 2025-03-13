7ae7c97209100d4f47c56f234c37154a04c747bc
Alpha <suntingting28@meituan.com>
2025-03-13 14:56:23 +0800

    pathC

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const PUBLIC_PATH = process.env.PUBLIC_PATH
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: PUBLIC_PATH,
 })
